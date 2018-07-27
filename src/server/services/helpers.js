7c84e8619181792140f19a0a7b1cd1c6cf99177a
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2018-07-27 11:58:01 +0100

    created graphql schema and types

---
+++ src/server/services/helpers.js
@@ -0,0 +1,13 @@
+import sql from "mssql";
+import { config } from "../../../config.env";
+
+export const generalQuery = async yourQuery => {
+  try {
+    const pool = await sql.connect(config);
+    const result = await pool.request().query(yourQuery);
+    return result.recordset;
+  } catch (err) {
+    console.log("error", err);
+  }
+  sql.on(`error`, err => console.log(err));
+};
