060a6602edf2ec6bee13bff970c550dcaa3012b7
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2018-07-27 17:38:18 +0100

    updated readme

--- src/server/services/helpers.js
+++ src/server/services/helpers.js
@@ -12,24 +12,3 @@ export const generalQuery = async yourQuery => {
   }
   sql.on(`error`, err => console.log(err));
 };
-
-// const sql = require("mssql");
-// const config = {
-//   user: "ReedCoUk",
-//   password: "R33dR0ck52014",
-//   server: "10.73.147.144",
-//   database: "Reed Online"
-// };
-
-// const generalQuery = async yourQuery => {
-//   try {
-//     const pool = await sql.connect(config);
-//     const result = await pool.request().query(yourQuery);
-//     return result.recordset;
-//   } catch (err) {
-//     console.log("error", err);
-//   }
-//   sql.on(`error`, err => console.log(err));
-// };
-
-// generalQuery(`Select * from Jobs Where Job_ID=${27313946}`).then(console.log);
