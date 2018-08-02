5dba9db2a7bf141c7c9e09a4aba60f63182f2b38
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2018-08-02 16:15:13 +0100

    removed the webpack-middleware as couldn't get the HMR working

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,7 @@ import { generalQuery } from "./services/helpers";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 app.use(express.static("dist"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "..", "dist", "index.html"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const outputDirectory = "dist";
@@ -11,6 +10,7 @@ module.exports = {
     filename: "[name].[hash].js",
     publicPath: "/"
   },
+
   mode: "development",
   module: {
     rules: [
